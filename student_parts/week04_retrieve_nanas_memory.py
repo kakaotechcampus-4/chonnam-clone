a611ac64be1245ebc72b43aaf7fc7decbbe03abb
kiki03st <rkdtjddbs0347@gmail.com>
2026-07-22 17:28:39 +0900

    search_personal_reference_hits 함수 작성

--- student_parts/week04_retrieve_nanas_memory.py
+++ student_parts/week04_retrieve_nanas_memory.py
@@ -245,8 +245,16 @@ def search_personal_reference_hits(
 ) -> list[dict[str, Any]]:
     """ChromaDB 검색 결과를 tool이 바로 반환하기 쉬운 hit 구조로 정리합니다."""
 
-    # TODO: 개인 참고자료 검색 결과를 id/content/distance/metadata 구조로 정리하세요.
-    ...
+    raw_hits = reference_store.search_personal_references(query, limit=top_k)
+    return [
+        {
+            "id": hit["id"],
+            "content": hit["content"],
+            "distance": hit["distance"],
+            "metadata": {"title": hit["title"], "tags": hit["tags"]},
+        }
+        for hit in raw_hits
+    ]
 
 
 def search_saved_request_rows(
