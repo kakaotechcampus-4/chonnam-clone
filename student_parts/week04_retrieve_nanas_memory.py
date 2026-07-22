912918f367b1517c78dc8eb419ad3dd1a420a646
kiki03st <rkdtjddbs0347@gmail.com>
2026-07-22 17:34:27 +0900

    search_saved_request_rows 함수 작성

--- student_parts/week04_retrieve_nanas_memory.py
+++ student_parts/week04_retrieve_nanas_memory.py
@@ -265,8 +265,7 @@ def search_saved_request_rows(
 ) -> list[dict[str, Any]]:
     """SQLite 저장 요청을 검색하고 실제 검색 결과만 반환합니다."""
 
-    # TODO: AppSQLiteStore.search_saved_requests(...)로 저장 요청을 검색하세요.
-    ...
+    return sqlite_store.search_saved_requests(query, limit=top_k)
 
 
 def search_conversation_messages_dict(
